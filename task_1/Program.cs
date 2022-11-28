c496b2f46d31257d4e6436c127ed1bcf24ffe56c
blazhkevych.v <blazhkevych.v@gmail.com>
2022-11-28 21:09:00 +0200

    Добавьте файлы проекта.

---
+++ task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace task_1
+{
+    /// <summary>
+    /// 1. Написать приложение, проверяющее с помощью регулярного выражения корректность ввода
+    /// фамилии и инициалов пользователя в следующем виде: Иванов И.И. либо Иванов ИИ
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace task_3
+{
+    /// <summary>
+    /// 3. Написать приложение, проверяющее с помощью регулярного выражения корректность ввода даты в формате Число-МесяцГод (например, 01-04-2015). 
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
