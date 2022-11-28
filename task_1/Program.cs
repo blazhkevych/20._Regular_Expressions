741109e72d379c72b984154b5825b75a0048cd8b
blazhkevych.v <blazhkevych.v@gmail.com>
2022-11-28 21:30:10 +0200

    s

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace task_1
+﻿using System.Text.RegularExpressions;
+
+namespace task_1
 {
     /// <summary>
     /// 1. Написать приложение, проверяющее с помощью регулярного выражения корректность ввода
@@ -8,7 +10,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите фамилию и инициалы: ");
+            string input = Console.ReadLine();
+            Regex regexEngShort1 = new Regex(@"^[A-Z]{1}[a-z]{5} [A-Z]{2}$");
+            Regex regexEngLong2 = new Regex(@"^[A-Z]{1}[a-z]{5} [A-Z]{1}[.]{1}[A-Z]{1}[.]{1}$");
+            Regex regexCyrillicShort1 = new Regex(@"^[А-Я]{1}[а-я]{5} [А-Я]{2}$");
+            Regex regexCyrillicLong2 = new Regex(@"^[А-Я]{1}[а-я]{5} [Я-Я]{1}[.]{1}[А-Я]{1}[.]{1}$");
+            if (
+                regexEngShort1.IsMatch(input) || regexEngLong2.IsMatch(input) ||
+                regexCyrillicShort1.IsMatch(input) || regexCyrillicLong2.IsMatch(input)
+                )
+            {
+                Console.WriteLine("Введенные данные корректны");
+            }
+            else
+            {
+                Console.WriteLine("Введенные данные некорректны");
+            }
         }
     }
 }
