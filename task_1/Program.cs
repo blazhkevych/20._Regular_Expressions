025a49f288830b4ac56692f4a3135ef9e68f2397
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-11-29 10:54:57 +0200

    ы

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,32 +1,37 @@
 ﻿using System.Text.RegularExpressions;
 
-namespace task_1
+namespace task_1;
+
+/// <summary>
+///     1. Написать приложение, проверяющее с помощью регулярного выражения корректность ввода
+///     фамилии и инициалов пользователя в следующем виде: Иванов И.И. либо Иванов ИИ
+///     Иванов И.И. Иванов ИИ   Ivanov II Ivanov I.I.
+/// </summary>
+internal class Program
 {
-    /// <summary>
-    /// 1. Написать приложение, проверяющее с помощью регулярного выражения корректность ввода
-    /// фамилии и инициалов пользователя в следующем виде: Иванов И.И. либо Иванов ИИ
-    /// </summary>
-    internal class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
+        while (true)
         {
             Console.WriteLine("Введите фамилию и инициалы: ");
-            string input = Console.ReadLine();
-            Regex regexEngShort1 = new Regex(@"^[A-Z]{1}[a-z]{5} [A-Z]{2}$");
-            Regex regexEngLong2 = new Regex(@"^[A-Z]{1}[a-z]{5} [A-Z]{1}[.]{1}[A-Z]{1}[.]{1}$");
-            Regex regexCyrillicShort1 = new Regex(@"^[А-Я]{1}[а-я]{5} [А-Я]{2}$");
-            Regex regexCyrillicLong2 = new Regex(@"^[А-Я]{1}[а-я]{5} [Я-Я]{1}[.]{1}[А-Я]{1}[.]{1}$");
+            var input = Console.ReadLine();
+            // Проверка на пустую строку.
+            if (input == string.Empty)
+            {
+                Console.WriteLine("Вы ввели пустую строку!");
+                continue;
+            }
+
+            // Проверка на корректность ввода.
             if (
-                regexEngShort1.IsMatch(input) || regexEngLong2.IsMatch(input) ||
-                regexCyrillicShort1.IsMatch(input) || regexCyrillicLong2.IsMatch(input)
-                )
-            {
+                Regex.IsMatch(input, @"^[А-Я][а-я]+ [А-Я]\.?[А-Я]\.$") ||
+                Regex.IsMatch(input, @"^[А-Я][а-я]+ [А-Я][А-Я]$") ||
+                Regex.IsMatch(input, @"^[A-Z][a-z]+ [A-Z]\.?[A-Z]\.$") ||
+                Regex.IsMatch(input, @"^[A-Z][a-z]+ [A-Z][A-Z]$")
+            )
                 Console.WriteLine("Введенные данные корректны");
-            }
             else
-            {
                 Console.WriteLine("Введенные данные некорректны");
-            }
         }
     }
 }
